518379f5e130042d6b032ad240ef7388c9868c3f
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-07-04 07:39:11 -0400

    oto commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,26 +7,37 @@ import Swiper from 'swiper';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit {
+
+  private currentSlideIndex: number = 0;
+
   constructor() {}
 
   ngAfterViewInit() {
     new Swiper('.swiper', {
-      // Initialize Swiper with options
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true, // Make the dots clickable
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '"></span>'; // Default is bullets
-          }
-      },
       loop: true,
       centeredSlides: true,
       speed: 400,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+      slidesPerView: 1,
+      on: {
+        transitionEnd: () => {
+          this.currentSlideIndex = (this.currentSlideIndex + 1) % 3; // Assuming you have 3 slides
+          this.updatePagination(this.currentSlideIndex);
+        },
       },
-      slidesPerView: 1
+    });
+  }
+
+  updatePagination(index: number) {
+    const indicators = document.querySelectorAll('.pagination-indicator');
+    indicators.forEach((indicator, i) => {
+      if (i === index) {
+        indicator.classList.remove('w-3', 'md:w-4', 'lg:w-5', 'bg-[#D8E8E7]', 'dark:bg-[#8C8586]');
+        indicator.classList.add('w-8', 'md:w-10', 'lg:w-12', 'bg-black', 'dark:bg-black');
+      } else {
+        indicator.classList.remove('w-8', 'md:w-10', 'lg:w-12', 'bg-black', 'dark:bg-black');
+        indicator.classList.add('w-3', 'md:w-4', 'lg:w-5', 'bg-[#D8E8E7]', 'dark:bg-[#8C8586]');
+      }
     });
   }
 }
+
