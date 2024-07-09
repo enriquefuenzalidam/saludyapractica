d0250d78a6072296524115a4a33f4600e240adbe
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-07-09 07:11:15 -0400

    footer routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared.module'; 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 
 @NgModule({
   declarations: [AppComponent],
